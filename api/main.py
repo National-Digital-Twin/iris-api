1dc787969865a2d47b06997292a286b633cbfd7d
Sam Hill <sam.hill@informed.com>
2025-03-26 14:49:30 +0000

    OSS additions for iris-api repository

--- api/main.py
+++ api/main.py
@@ -37,3 +37,7 @@ app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(port))
+
+# SPDX-License-Identifier: Apache-2.0
+# © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
+# and is legally attributed to the Department for Business and Trade (UK) as the governing entity.

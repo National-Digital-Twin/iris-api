9e05450d61bacec4c8ebb13fe2738e2c724cbbcc
kingarj <alice.king@informed.com>
2025-04-14 12:53:52 +0100

    [DPAV-725] update changelog

--- api/query.py
+++ api/query.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
+# and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
+
 def get_building(uprn: str) -> str:
     return f"""
         PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>

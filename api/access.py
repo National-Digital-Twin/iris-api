228e1b5ee0cb6ba8b7c354ffe1c25e391d7c5909
KumailKamranIS <kumail.kamran@informed.com>
2025-04-16 11:06:48 +0100

    bugfix(dpav-589): fixed and added proper exception handling to the new endpoints

--- api/access.py
+++ api/access.py
@@ -2,14 +2,17 @@
 # © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
 # and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
-import os
 import logging
+import os
+
 import requests
+from requests import codes, exceptions
 from utils import get_headers
 
 # Set up logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
 
 class AccessClient:
     def __init__(self, connection_string: str, dev_mode: bool):
@@ -18,7 +21,7 @@ class AccessClient:
 
         # Check for the environment variable at startup.
         if not self.dev:
-            self.IDENTITY_API_URL = os.environ.get('IDENTITY_API_URL')
+            self.IDENTITY_API_URL = os.environ.get("IDENTITY_API_URL")
             if not self.IDENTITY_API_URL:
                 logger.error("IDENTITY_API_URL environment variable is not set.")
                 raise KeyError("IDENTITY_API_URL environment variable is not set.")
@@ -27,26 +30,33 @@ class AccessClient:
         # If in dev mode, return dummy data.
         if self.dev:
             logger.info("Dev mode enabled, returning dummy user details.")
-            return {"username": "Test User1", "user_id": "1234-5678-99ab-cdef"}
-        
+            return {
+                "username": "Test User1",
+                "user_id": "1234-5678-99ab-cdef",
+                "email": "test.user@example.com",
+            }
+
         # Build the full URL for the API call.
         url = f"{self.IDENTITY_API_URL}/api/v1/user-details"
         logger.info("Making API call to %s", url)
 
         try:
             res = requests.get(url, headers=get_headers(headers))
-        except Exception as e:
+        except exceptions.RequestException as e:
             logger.exception("Error making API call: %s", e)
-            return {"error": "API call failed", "details": str(e)}
+            raise e
 
-        if res.status_code == 200:
+        if res.status_code == codes.ok:
             data = res.json()
             logger.info("Received successful response from API")
-            # Map displayName to username and username to user_id.
+            # Map displayName to username, username to user_id and email to email.
             return {
                 "username": data["content"]["displayName"],
-                "user_id": data["content"]["username"]
+                "user_id": data["content"]["username"],
+                "email": data["content"]["email"],
             }
         else:
-            logger.error("API call returned error status %s: %s", res.status_code, res.text)
-            return res.json()
+            logger.error(
+                "API call returned error status %s: %s", res.status_code, res.text
+            )
+            res.raise_for_status()

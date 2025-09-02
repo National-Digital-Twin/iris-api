bc1b3f626e053b8462d16bfd9a006e145b2320ef
MattThomas-Informed <matthew.thomas@informed.com>
2025-09-02 09:58:44 +0100

    Update d99eb3e1e4ab_011_create_uk_counties_table_view.py
    uk ward referenced now removed

--- api/alembic/versions/d99eb3e1e4ab_011_create_uk_counties_table_view.py
+++ api/alembic/versions/d99eb3e1e4ab_011_create_uk_counties_table_view.py
@@ -96,7 +96,7 @@ def upgrade() -> None:
                 COUNT(CASE WHEN a.epc_rating IS NULL THEN 1 END) AS epc_null,
                 b.geometry
             FROM iris.building_epc a
-            LEFT JOIN iris.uk_ward b
+            LEFT JOIN iris.boundary_line_ceremonial_counties b
             ON ST_Intersects(b.geometry, a.point)
             GROUP BY b.name, b.geometry;
     """

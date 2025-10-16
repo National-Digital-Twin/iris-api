a208f052de22d868af70f4164c43df1c85afc90a
KumailKamranIS <131384933+KumailKamranIS@users.noreply.github.com>
2025-10-16 11:38:55 +0100

    Revert "revert(non-req): revert changes to handle nullable roof aspect area data (#114)" (#115)
    This reverts commit 38a2ea6493e09d436c4e0b3e895bfd94150c79d0.

--- api/utils.py
+++ api/utils.py
@@ -7,6 +7,7 @@ pass_through_headers = [
     "Authorization",
 ]
 
+
 def get_headers(headers):
     forward_headers = {}
     for header in pass_through_headers:
@@ -15,5 +16,6 @@ def get_headers(headers):
             forward_headers[header] = hv
     return forward_headers
 
-def has_bindings(r): 
+
+def has_bindings(r):
     return bool(r and r.get("results") and r.get("results").get("bindings"))

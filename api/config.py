fbea875165664a9759b755073e86db1b1f33bb65
KumailKamranIS <131384933+KumailKamranIS@users.noreply.github.com>
2026-01-22 16:46:12 +0000

    feat(non-req): add match_score column to the epc assessment table (#164)
    * feat(non-req): add match_score column to the epc assessment table
    
    * feat(OSPO): synchronise OSPO workflows
    
    * feat(non-req): addressed review comments

--- api/config.py
+++ api/config.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     DB_USERNAME: str
     DB_PASSWORD: str
     DB_HOST: str
+    DB_PORT: int = 5432
 
     JENA_URL: str = "localhost"
     JENA_PORT: int = 3030
@@ -39,7 +40,7 @@ class Settings(BaseSettings):
         if self.ENVIRONMENT == "TEST":
             return None
         else:
-            return f"postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:5432/iris"
+            return f"postgresql+asyncpg://{self.DB_USERNAME}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/iris"
 
 
 @lru_cache

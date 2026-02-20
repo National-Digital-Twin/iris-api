6e28c536dc2c49627ae48c305d5cdba34ecffcee
KumailKamranIS <kumail.kamran@informed.com>
2026-02-20 13:08:15 +0000

    bugfix(dpav-2484): fixed the ONS deprivation data resources migration

--- api/alembic/versions/f4b7c2d9a1e3_039_create_deprivation_lookup_tables_and_mv.py
+++ api/alembic/versions/f4b7c2d9a1e3_039_create_deprivation_lookup_tables_and_mv.py
@@ -9,6 +9,7 @@ Revises: d19f556331db
 Create Date: 2026-02-17 16:30:00.000000
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -22,22 +23,19 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     """Upgrade schema."""
-    op.execute(
-        """
+    op.execute("""
         CREATE SEQUENCE IF NOT EXISTS iris.msoa_boundaries_id_seq
             INCREMENT 1
             START 1
             MINVALUE 1
             MAXVALUE 2147483647
             CACHE 1;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE TABLE IF NOT EXISTS iris.msoa_boundaries
         (
-            id integer NOT NULL DEFAULT nextval('msoa_boundaries_id_seq'::regclass),
+            id integer NOT NULL DEFAULT nextval('iris.msoa_boundaries_id_seq'::regclass),
             msoa21cd character varying(9) COLLATE pg_catalog."default",
             msoa21nm character varying(39) COLLATE pg_catalog."default",
             msoa21nmw character varying(29) COLLATE pg_catalog."default",
@@ -50,48 +48,34 @@ def upgrade() -> None:
             CONSTRAINT msoa_boundaries_pkey PRIMARY KEY (id)
         )
         TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.msoa_boundaries
-            OWNER to iris;
-        """
-    )
-    op.execute(
-        """
+    op.execute("""
         ALTER SEQUENCE IF EXISTS iris.msoa_boundaries_id_seq
             OWNED BY iris.msoa_boundaries.id;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE INDEX IF NOT EXISTS msoa_boundaries_geom_geom_idx
             ON iris.msoa_boundaries USING gist
             (geom)
             WITH (fillfactor=90, buffering=auto)
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE SEQUENCE IF NOT EXISTS iris.oa_boundaries_analytics_id_seq
             INCREMENT 1
             START 1
             MINVALUE 1
             MAXVALUE 2147483647
             CACHE 1;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE TABLE IF NOT EXISTS iris.oa_boundaries_analytics
         (
-            id integer NOT NULL DEFAULT nextval('oa_boundaries_analytics_id_seq'::regclass),
+            id integer NOT NULL DEFAULT nextval('iris.oa_boundaries_analytics_id_seq'::regclass),
             oa21cd character varying(9) COLLATE pg_catalog."default",
             lsoa21cd character varying(9) COLLATE pg_catalog."default",
             lsoa21nm character varying(40) COLLATE pg_catalog."default",
@@ -105,44 +89,30 @@ def upgrade() -> None:
             CONSTRAINT oa_boundaries_analytics_pkey PRIMARY KEY (id)
         )
         TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.oa_boundaries_analytics
-            OWNER to iris;
-        """
-    )
-    op.execute(
-        """
+    op.execute("""
         ALTER SEQUENCE IF EXISTS iris.oa_boundaries_analytics_id_seq
             OWNED BY iris.oa_boundaries_analytics.id;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE INDEX IF NOT EXISTS idx_oa_geom_gist
             ON iris.oa_boundaries_analytics USING gist
             (geom)
             WITH (fillfactor=90, buffering=auto)
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE INDEX IF NOT EXISTS oa_boundaries_analytics_geom_geom_idx
             ON iris.oa_boundaries_analytics USING gist
             (geom)
             WITH (fillfactor=90, buffering=auto)
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE TABLE IF NOT EXISTS iris.ons_deprivation_metrics
         (
             mlsoa_id text COLLATE pg_catalog."default",
@@ -154,18 +124,9 @@ def upgrade() -> None:
             dep_4 integer
         )
         TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.ons_deprivation_metrics
-            OWNER to iris;
-        """
-    )
-
-    op.execute(
-        """
+    op.execute("""
         CREATE TABLE IF NOT EXISTS iris.ons_deprivation_metrics_analytics
         (
             oa_id text COLLATE pg_catalog."default",
@@ -177,18 +138,9 @@ def upgrade() -> None:
             dep_4 integer
         )
         TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.ons_deprivation_metrics_analytics
-            OWNER to iris;
-        """
-    )
-
-    op.execute(
-        """
+    op.execute("""
         CREATE MATERIALIZED VIEW IF NOT EXISTS iris.msoa_deprivation_data
         TABLESPACE pg_default
         AS
@@ -225,36 +177,23 @@ def upgrade() -> None:
         FROM iris.ons_deprivation_metrics m
             JOIN iris.msoa_boundaries b ON m.mlsoa_id = b.msoa21cd::text
         WITH DATA;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.msoa_deprivation_data
-            OWNER TO iris;
-        """
-    )
-
-    op.execute(
-        """
+    op.execute("""
         CREATE INDEX IF NOT EXISTS msoa_deprivation_data_geom_gix
             ON iris.msoa_deprivation_data USING gist
             (geom)
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE UNIQUE INDEX IF NOT EXISTS msoa_deprivation_data_pk
             ON iris.msoa_deprivation_data USING btree
             (mlsoa_id COLLATE pg_catalog."default")
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE MATERIALIZED VIEW IF NOT EXISTS iris.msoa_deprivation_geojson
         TABLESPACE pg_default
         AS
@@ -297,18 +236,9 @@ def upgrade() -> None:
         SELECT json_build_object('type', 'FeatureCollection', 'features', json_agg(json_build_object('type', 'Feature', 'geometry', st_asgeojson(st_transform(st_setsrid(geom_simpl, 27700), 4326), 5)::json, 'properties', (to_jsonb(cov.*) - 'geom_27700'::text - 'geom_simpl'::text)::json))) AS geojson
         FROM cov
         WITH DATA;
-        """
-    )
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.msoa_deprivation_geojson
-            OWNER TO iris;
-        """
-    )
-
-    op.execute(
-        """
+    op.execute("""
         CREATE MATERIALIZED VIEW IF NOT EXISTS iris.oa_area_membership_mv
         TABLESPACE pg_default
         AS
@@ -324,115 +254,71 @@ def upgrade() -> None:
             JOIN iris.building_epc_analytics bea ON oa.geom_4326 && bea.point AND st_contains(oa.geom_4326, bea.point)
             CROSS JOIN LATERAL ( VALUES ('region'::text,bea.region_name), ('county'::text,bea.county_name), ('district'::text,bea.district_name), ('ward'::text,bea.ward_name)) v(area_level, area_name)
         WHERE v.area_name IS NOT NULL
-        WITH DATA;
-        """
-    )
+        WITH NO DATA;
+        """)
 
-    op.execute(
-        """
-        ALTER TABLE IF EXISTS iris.oa_area_membership_mv
-            OWNER TO iris;
-        """
-    )
-
-    op.execute(
-        """
+    op.execute("""
         CREATE INDEX IF NOT EXISTS idx_oa_area_membership_mv_lookup
             ON iris.oa_area_membership_mv USING btree
             (area_level COLLATE pg_catalog."default", area_name COLLATE pg_catalog."default", oa21cd COLLATE pg_catalog."default")
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
-    op.execute(
-        """
+    op.execute("""
         CREATE UNIQUE INDEX IF NOT EXISTS idx_oa_area_membership_mv_uq
             ON iris.oa_area_membership_mv USING btree
             (oa21cd COLLATE pg_catalog."default", area_level COLLATE pg_catalog."default", area_name COLLATE pg_catalog."default")
             TABLESPACE pg_default;
-        """
-    )
+        """)
 
 
 def downgrade() -> None:
     """Downgrade schema."""
-    op.execute(
-        """
+    op.execute("""
         DROP INDEX IF EXISTS iris.idx_oa_area_membership_mv_lookup;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.idx_oa_area_membership_mv_uq;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP MATERIALIZED VIEW IF EXISTS iris.oa_area_membership_mv;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP MATERIALIZED VIEW IF EXISTS iris.msoa_deprivation_geojson;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.msoa_deprivation_data_geom_gix;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.msoa_deprivation_data_pk;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP MATERIALIZED VIEW IF EXISTS iris.msoa_deprivation_data;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP TABLE IF EXISTS iris.ons_deprivation_metrics_analytics;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP TABLE IF EXISTS iris.ons_deprivation_metrics;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.idx_oa_geom_gist;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.oa_boundaries_analytics_geom_geom_idx;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP TABLE IF EXISTS iris.oa_boundaries_analytics;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP SEQUENCE IF EXISTS iris.oa_boundaries_analytics_id_seq;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP INDEX IF EXISTS iris.msoa_boundaries_geom_geom_idx;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP TABLE IF EXISTS iris.msoa_boundaries;
-        """
-    )
-    op.execute(
-        """
+        """)
+    op.execute("""
         DROP SEQUENCE IF EXISTS iris.msoa_boundaries_id_seq;
-        """
-    )
+        """)

8361d8753845727721fdd48a0476c700375b2473
Ethan Higgins <ethan.higgins@informed.com>
2026-02-24 10:22:43 +0000

    feat(dpav-2497): adjust migration to now only add sunlight hours to bwa view

--- api/alembic/versions/db527e1274ec_040_add_sunlight_hours_to_building_weather_analytics_view.py
+++ api/alembic/versions/db527e1274ec_040_add_sunlight_hours_to_building_weather_analytics_view.py
@@ -2,7 +2,7 @@
 # © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme
 # and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
 
-"""040_remove_dependancy_on_epc_view
+"""040_add_sunlight_hours_to_building_weather_analytics_view
 
 Revision ID: db527e1274ec
 Revises: f4b7c2d9a1e3
@@ -107,11 +107,7 @@ def _create_building_weather_analytics_view() -> None:
                 wdrp_315.wdr_40_median as wdr_40_median_315,
                 acohdp.hsd_40_median,
                 aacosh.sunlight_hours as average_annual_sunlight_hours,
-                aacosh.sunlight_hours / 365.0 as average_daily_sunlight_hours,
-                COALESCE(er.name, sawr.name) AS region_name,
-                blcc.name AS county_name,
-                dbu.name AS district_name,
-                COALESCE(dbuw.name, ued.name) AS ward_name
+                aacosh.sunlight_hours / 365.0 as average_daily_sunlight_hours
             FROM iris.building b
             JOIN iris.annual_count_of_icing_days_1991_2020 acoid ON ST_CONTAINS(acoid.shape, b.point)
             JOIN wdrp_0 ON ST_CONTAINS(wdrp_0.shape, b.point)
@@ -123,13 +119,7 @@ def _create_building_weather_analytics_view() -> None:
             JOIN wdrp_270 ON ST_CONTAINS(wdrp_270.shape, b.point)
             JOIN wdrp_315 ON ST_CONTAINS(wdrp_315.shape, b.point)
             JOIN iris.annual_count_of_hot_summer_days_projections_12km acohdp ON ST_CONTAINS(acohdp.shape, b.point)
-            JOIN iris.boundary_line_ceremonial_counties blcc ON st_contains(blcc.geometry, b.point)
-            JOIN iris.district_borough_unitary dbu ON st_contains(dbu.geometry, b.point)
             JOIN iris.average_annual_count_of_sunlight_hours_5km aacosh ON ST_CONTAINS(aacosh.shape, b.point)
-            LEFT JOIN iris.district_borough_unitary_ward dbuw ON st_contains(dbuw.geometry, b.point)
-            LEFT JOIN iris.unitary_electoral_division ued ON st_contains(ued.geometry, b.point)
-            LEFT JOIN iris.english_region er ON er.fid = dbu.english_region_fid
-            LEFT JOIN iris.scotland_and_wales_region sawr ON sawr.fid = dbu.scotland_and_wales_region_fid
             WHERE b.is_residential = true
         )
         WITH NO DATA;
@@ -144,123 +134,6 @@ def _drop_building_weather_analytics_view() -> None:
     )
 
 def _create_building_extreme_weather_analytics_view() -> None:
-    op.execute(
-        """
-        CREATE MATERIALIZED VIEW iris.building_extreme_weather_analytics AS (
-            WITH buildings_affected_by_icing_days AS (
-                SELECT uprn, true AS affected
-                FROM iris.building_weather_analytics
-                WHERE icing_days > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY icing_days
-                    )
-                    FROM iris.building_weather_analytics
-                )
-            ),
-            buildings_affected_by_hsds AS (
-                SELECT uprn, true AS affected
-                FROM iris.building_weather_analytics
-                WHERE hsd_40_median > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY hsd_40_median
-                    )
-                    FROM iris.building_weather_analytics
-                )
-            ),
-            buildings_affected_by_wdrp AS (
-                SELECT uprn, true AS affected
-                FROM iris.building_weather_analytics
-                WHERE wdr_40_median_0 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_0
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_45 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_45
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_90 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_90
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_135 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_135
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_180 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_180
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_225 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_225
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_270 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_270
-                    )
-                    FROM iris.building_weather_analytics
-                )
-                OR wdr_40_median_315 > (
-                    SELECT percentile_cont(0.75) WITHIN GROUP (
-                        ORDER BY wdr_40_median_315
-                    )
-                    FROM iris.building_weather_analytics
-                )
-            )
-            SELECT DISTINCT ON (bwa.uprn)
-                bwa.uprn,
-                bwa.point,
-                babi.affected as affected_by_icing_days,
-                babh.affected as affected_by_hsds,
-                babw.affected as affected_by_wdr,
-                bwa.region_name,
-                bwa.county_name,
-                bwa.district_name,
-                bwa.ward_name
-            FROM iris.building_weather_analytics bwa
-            LEFT JOIN buildings_affected_by_icing_days babi ON bwa.uprn = babi.uprn
-            LEFT JOIN buildings_affected_by_hsds babh ON bwa.uprn = babh.uprn
-            LEFT JOIN buildings_affected_by_wdrp babw ON bwa.uprn = babw.uprn
-            LEFT JOIN iris.building_epc_analytics bea ON bwa.uprn = bea.uprn
-            ORDER BY bwa.uprn, bea.lodgement_date DESC NULLS LAST
-        )
-        WITH NO DATA;
-    """
-    )
-
-def _drop_building_extreme_weather_analytics_view() -> None:
-    op.execute(
-        """
-        DROP MATERIALIZED VIEW IF EXISTS iris.building_extreme_weather_analytics;
-        """
-    )
-
-
-def upgrade() -> None:
-    """Upgrade schema."""
-    _drop_building_extreme_weather_analytics_view()
-    _drop_building_weather_analytics_view_indices()
-    _drop_building_weather_analytics_view()
-    _create_building_weather_analytics_view()
-    _create_building_weather_analytics_view_indices()
-    _create_building_extreme_weather_analytics_view()
-    # ### end Alembic commands ###
-
-
-
-def _create_downgrade_building_extreme_weather_analytics_view():
     op.execute(
         """
         CREATE MATERIALIZED VIEW iris.building_extreme_weather_analytics AS (
@@ -357,6 +230,24 @@ def _create_downgrade_building_extreme_weather_analytics_view():
     """
     )
 
+def _drop_building_extreme_weather_analytics_view() -> None:
+    op.execute(
+        """
+        DROP MATERIALIZED VIEW IF EXISTS iris.building_extreme_weather_analytics;
+        """
+    )
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    _drop_building_extreme_weather_analytics_view()
+    _drop_building_weather_analytics_view_indices()
+    _drop_building_weather_analytics_view()
+    _create_building_weather_analytics_view()
+    _create_building_weather_analytics_view_indices()
+    _create_building_extreme_weather_analytics_view()
+    # ### end Alembic commands ###
+
 def _create_downgrade_building_weather_analytics_view() -> None:
     op.execute(
         """
@@ -441,7 +332,6 @@ def downgrade() -> None:
     _drop_building_weather_analytics_view()
     _create_downgrade_building_weather_analytics_view()
     _create_building_weather_analytics_view_indices()
-    _create_downgrade_building_extreme_weather_analytics_view()
+    _create_building_extreme_weather_analytics_view()
 
-    pass
     # ### end Alembic commands ###
